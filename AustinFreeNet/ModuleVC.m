50bfc2985f14e3ba008136ac09b65204cc644de4
Kelsey Mayfield <kelsey.mayfield@gmail.com>
2015-04-15 12:53:24 -0500

    added PDF view

--- AustinFreeNet/ModuleVC.m
+++ AustinFreeNet/ModuleVC.m
@@ -9,7 +9,8 @@
 #import "ModuleVC.h"
 
 @interface ModuleVC()
-@property (weak, nonatomic) IBOutlet UIWebView *videoView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ModuleVC
@@ -18,14 +19,17 @@
 {
 	[super viewDidLoad];
 	self.title = self.moduleInfo[@"name"];
+	NSURL *fileURL = [NSURL URLWithString:self.moduleInfo[@"data"]];
+	[self.spinner startAnimating];
 	if([self.moduleInfo[@"type"] isEqualToString:@"YouTube"]) {
-		[self embedVideo];
+		[self embedVideo:fileURL];
+	} else if ([self.moduleInfo[@"type"] isEqualToString:@"PDF"]) {
+		[self embedPDF:fileURL];
 	}
 }
 
-- (void)embedVideo
+- (void)embedVideo:(NSURL *)url
 {
-	NSURL *fileURL = [NSURL URLWithString:@"http://www.youtube.com/watch?v=9bZkp7q19f0"];
 	NSString* embedHTML = @"\
 	<html><head>\
  <style type=\"text/css\">\
@@ -38,9 +42,30 @@
 	<embed id=\"yt\" src=\"%@\" type=\"application/x-shockwave-flash\" \
  width=\"%0.0f\" height=\"%0.0f\"></embed>\
 	</body></html>";
-	 NSString* html = [NSString stringWithFormat:embedHTML, fileURL, self.videoView.frame.size.width, self.videoView.frame.size.height];
+	 NSString* html = [NSString stringWithFormat:embedHTML, url, self.webView.frame.size.width, self.webView.frame.size.height];
 
-	 [self.videoView loadHTMLString:html baseURL:nil];
+	 [self.webView loadHTMLString:html baseURL:nil];
+}
+
+- (void)embedPDF:(NSURL *)url
+{
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	NSURLSessionConfiguration *config = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+	NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+	NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+		if (!error) {
+			if ([request.URL isEqual:url]) {
+				NSData *data = [NSData dataWithContentsOfURL:location];
+				dispatch_async(dispatch_get_main_queue(), ^{
+					[self.webView loadData:data MIMEType:@"application/pdf" textEncodingName:@"utf-8" baseURL:nil];
+					[self.spinner stopAnimating];
+				});
+				
+			}
+		}
+	}];
+	[task resume];
+//	[self.webView loadRequest:request];
 }
 
 @end
